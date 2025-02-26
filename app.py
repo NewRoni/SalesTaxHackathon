ac50281881e0383af3ad84d9a2d51a630c47e818
Louis <l.j.k.gunther-mcgee1@newcastle.ac.uk>
2025-02-26 14:24:57 +0000

    made an input box

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def Main():
-    return render_template('Main.html')
+    result = None
+    if request.method == 'POST':
+        try:
+            text = request.form['State']
+            text = String(text)
+        except ValueError:
+            result = "invalid input"
+
+    return render_template('Main.html', result=result)
+
+
 
 if __name__ == "__main__":
     app.run(use_reloader = True, debug=True, host="0.0.0.0")
